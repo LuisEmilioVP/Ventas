0ff518965d72c74747f95c56dd41668ffac30f25
Jesus-Adriantf <jesusadriantraviesofernandez@gmail.com>
2023-06-02 23:50:16 -1200

    Estructura de capa de Domian y Infrastructure

---
+++ Ventas.Domain/Entities/Suplidor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Ventas.Domain.Core;
+
+namespace Ventas.Domain.Entities
+{
+    public class Suplidor : Persona
+    {
+
+        public int? idSuplidor { get; set; }
+        public string? contact { get; set; }
+        public string? city { get; set; }
+        public string region { get; set; }
+        public string Postal_Code { get; set; }
+        public string? country { get; set; }
+        public string fax { get; set; }
+
+    }
+}
---
+++ Ventas.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Ventas.Domain.Repository
+{
+    public interface  IBaseRepository<TEntity> where TEntity : class
+    {
+        //void para especificar que el método no devuelve un valor. 
+
+        void Save(TEntity entity);
+        void Save(TEntity[] entities);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity GetEntity(int entityid);
+        bool Exists(Expression<Func<TEntity, bool>> filter);
+        IEnumerable<TEntity> GetEntities();
+    }
+}
