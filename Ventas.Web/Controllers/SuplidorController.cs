e899b56b361652d3c0e71270de66f17ba7f87822
Jesus-Adriantf <jesusadriantraviesofernandez@gmail.com>
2023-07-29 02:22:14 -1200

    Agregando API a Presentacion

--- Ventas.Web/Controllers/SuplidorController.cs
+++ Ventas.Web/Controllers/SuplidorController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Ventas.Application.Contract;
 using Ventas.Application.Dto.Suplidor;
+using Ventas.Domain.Entities;
 using Ventas.Infrastructure.Models;
 using Ventas.Web.Models;
 
@@ -112,6 +113,42 @@ namespace Ventas.Web.Controllers
             }
         }
 
-       
+        // GET: kkkController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var suplidor = suplidorService.GetById(id);
+
+            if (!suplidor.Success)
+                ViewBag.Message = suplidor.Message;
+
+            var supliDelete = (SuplidorModels)suplidor.Data;
+
+            return View(supliDelete);
+        }
+
+        // POST: kkkController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(SuplidorRemoveDto suplidorRemoveDto)
+        {
+            try
+            {
+                var suplidor = this.suplidorService.Delete(suplidorRemoveDto);
+
+                if (!suplidor.Success)
+                {
+                    ViewBag.Message = suplidor.Message;
+                    return View(suplidor);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
     }
 }
